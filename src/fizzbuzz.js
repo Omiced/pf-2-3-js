8c0fe26842e3b0c79669cc518fe581e72baea995
Anghelo Ortiz <anghelotiz@gmail.com>
2025-08-25 12:30:56 -0600

    Se ajusto el Fuzz por Buzz

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,14 @@
 export function fizzbuzz(){
-  //tu código va aqui
-  
+for (let valor = 1; valor <=100; valor++){// corre del 1 al 100
+  if (valor %3===0 && valor %5===0) {
+    console.log("FizzBuzz");
+  }else if (valor %3===0){
+    console.log("Fizz");
+  }else if (valor %5===0){
+    console.log("Buzz");
+  }
+  else {
+    console.log(valor);
+  }
+}
 }
